e6a028d0a93477f6899e241beb7e99a6b00bde2a
shakram02 <ahmedhamdyau@gmail.com>
2021-11-02 22:14:49 +0200

    Fix classification utils

--- classification_utils.py
+++ classification_utils.py
@@ -1,3 +1,8 @@
+import os
+from common_utils import _get_timestamp_string
+from common_config import OUTPUT_DIRECTORY
+
+
 def make_classification_model_directory(model_name):
     ts = _get_timestamp_string()
     name = f"{model_name}_cls_{ts}_tile_448_448"
