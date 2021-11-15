27a9616f77321c05b727814163d4709f10b92438
shakram02 <ahmedhamdyau@gmail.com>
2021-11-15 09:49:57 +0200

    become a python package

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from all_classes_config import *
+from common_config import *
+from common_utils import *
+from detection_config import *
+from detection_utils import *
+from classification_config import *
+from classification_utils import *
