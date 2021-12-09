15278ec0d254fde6b1bbc673c961d1384ff2eefa
shakram02 <ahmedhamdyau@gmail.com>
2021-12-09 11:32:21 +0200

    print report table

--- classification_utils.py
+++ classification_utils.py
@@ -1,4 +1,6 @@
 import os
+
+from numpy import number
 from .common_utils import _get_timestamp_string
 from .common_config import OUTPUT_DIRECTORY
 
@@ -38,8 +40,15 @@ def print_metrics(confusion_m, classification_report, labels):
 
     print('Confusion matrix: \n', tabulated_matrix)
     print('Classification report : \n')
-    from pprint import pprint
-    pprint(classification_report)
+
+    for k, v in classification_report.items():
+        if isinstance(v, float):
+            print(k, '\n', v)
+        elif isinstance(v, dict):
+            print(k, '\n')
+            keys = ','.join(v.keys())
+            values = ','.join([str(x) for x in v.values()])
+            print(f'{keys}\n{values}')
 
 
 def compute_metrics(actual, predicted, labels):
@@ -47,13 +56,13 @@ def compute_metrics(actual, predicted, labels):
     from sklearn.metrics import classification_report
     import numpy as np
 
-    labels_np = np.array(labels)
-    p = labels_np[predicted]
-    a = labels_np[actual]
+    labels_np=np.array(labels)
+    p=labels_np[predicted]
+    a=labels_np[actual]
 
-    confusion_m = confusion_matrix(a, p, labels=labels)
-    report = classification_report(a, p, labels=labels)
+    confusion_m=confusion_matrix(a, p, labels=labels)
+    report=classification_report(a, p, labels=labels)
     print('Classification report:\n', report)
-    report = classification_report(a, p, labels=labels, output_dict=True)
+    report=classification_report(a, p, labels=labels, output_dict=True)
 
     return confusion_m, report
