e678d3ad5c9e3278316228e3f09fb0fae8ec7bbd
shakram02 <ahmedhamdyau@gmail.com>
2022-01-31 13:39:48 +0200

    Update model diretory name to include tile size

--- classification_utils.py
+++ classification_utils.py
@@ -5,11 +5,12 @@ import io
 from collections import Counter
 from .common_utils import _get_timestamp_string
 from .common_config import OUTPUT_DIRECTORY
+from .classification_config import TILE_SIDE_HEIGHT, TILE_SIDE_WIDTH
 
 
 def make_classification_model_directory(model_name):
     ts = _get_timestamp_string()
-    name = f"{model_name}_cls_{ts}_tile_448_448"
+    name = f"{model_name}_cls_{ts}_tile_{TILE_SIDE_WIDTH}_{TILE_SIDE_HEIGHT}"
     model_dir = os.path.join(OUTPUT_DIRECTORY, name)
 
     os.makedirs(model_dir)
